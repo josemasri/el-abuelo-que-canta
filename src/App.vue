dc366872c2e53669bd2623dff46cf8e96fedc0c7
Jose Masri <ae_jmsalame@contractor.indeed.com>
2023-10-03 13:13:12 -0600

    Es mi abuelo que canta

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>El abuelo que canta</h1>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/ZUr5STk_12Q?si=i-VhOJkFZA01VgIQ"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+/* Add your scoped CSS styles here */
+</style>
